5f66ef66650356c0e28867ea890dfdde58e854d0
Brian Lin <brianlin@umich.edu>
2021-03-30 18:25:09 +1300

    Initial Commit
    Add initial npm package info and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const path = require('path');
+
+module.exports = {
+    entry: {
+        content: path.resolve(__dirname, "src/content.js"),
+        popup: path.resolve(__dirname, "src/popup.js"),
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist/javascripts'),
+        filename: '[name].bundle.js',
+    },
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'vue-loader'
+            },
+            // this will apply to both plain `.js` files
+            // AND `<script>` blocks in `.vue` files
+            {
+                test: /\.js$/,
+                include: path.resolve(__dirname, 'src'),
+                loader: 'babel-loader'
+            },
+            // this will apply to both plain `.css` files
+            // AND `<style>` blocks in `.vue` files
+            {
+                test: /\.css$/,
+                include: path.resolve(__dirname, 'src'),
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ],
+            }
+        ]
+    },
+    plugins: [
+        // make sure to include the plugin for the magic
+        new VueLoaderPlugin()
+    ],
+}
