fb8d28ffe430dd36745dd9bf5aad8c0989cd4369
Brian Lin <brianlin@umich.edu>
2021-04-07 19:54:06 +1200

    Add unit tests for content script

---
+++ tests/content.test.js
@@ -0,0 +1,106 @@
+import { chrome } from 'jest-chrome';
+import { testables } from "../src/content.js";
+
+// We don't test changeTime as it's fairly trivial and the test uses 
+// almost the same code anyways.
+
+const {
+    getVideoId, getCurrentTimestampInfo, listenMessages
+} = testables;
+
+beforeAll(() => {
+    // mock window.location attributes
+    // https://stackoverflow.com/questions/54021037/how-to-mock-window-location-href-with-jest-vuejs
+    global.window = Object.create(window);
+    Object.defineProperty(window, 'location', {
+        value: {}
+    });
+
+    // mock the DOM of Youtube video page
+    document.body.innerHTML = ""
+    const body = document.body;
+    const video = document.createElement("video");
+    video.currentTime = 1;
+    body.appendChild(video);
+
+    const videoTitle = document.createElement("h1");
+    videoTitle.className = "title style-scope ytd-video-primary-info-renderer";
+    const innerVideoTitle = document.createElement("div");
+    innerVideoTitle.innerText = "video title"
+    videoTitle.appendChild(innerVideoTitle);
+    body.appendChild(videoTitle);
+});
+
+beforeEach(() => {
+    window.location.search = "https://www.youtube.com/watch?v=videoId&t=3";
+    // reset video's current time in case it was changed
+    const video = document.getElementsByTagName("video")[0];
+    video.currentTime = 1;
+})
+
+afterAll(() => {
+    document.body.innerHTML = "";
+})
+
+describe("getVideoId", () => {
+    test("url with only videoid in query string", () => {
+        window.location.search = "https://www.youtube.com/watch?v=videoId";
+        const result = getVideoId();
+        expect(result).toBe("videoId");
+    });
+
+    test("url with other fields in query string", () => {
+        let result = getVideoId();
+        expect(result).toBe("videoId");
+    });
+});
+
+describe("getCurrentTimestampInfo", () => {
+    test("DOM scraping of youtube video page", () => {
+        const info = getCurrentTimestampInfo();
+        expect(info).toEqual(
+            {timestamp: 1, videoId: "videoId", title: "video title"}
+        );
+    });
+});
+
+describe("listenMessages", () => {
+    beforeEach(() => {
+        chrome.runtime.onMessage.clearListeners();
+    });
+
+    afterAll(() => {
+        chrome.runtime.onMessage.clearListeners();
+    })
+
+    test("register message listener", () => {
+        // sanity check
+        expect(chrome.runtime.onMessage.hasListeners()).toBe(false);
+        listenMessages();
+        expect(chrome.runtime.onMessage.hasListeners()).toBe(true);
+    });
+
+    test("request obtain-timestamp", () => {
+        const request = { msg: 'obtain-timestamp' };
+        const sender = {};
+        const responseSpy = jest.fn((response) => {});
+
+        listenMessages();
+        chrome.runtime.onMessage.callListeners(request, sender, responseSpy);
+        expect(responseSpy.mock.calls.length).toBe(1);
+        expect(responseSpy.mock.calls[0][0]).toEqual(
+            {timestamp: 1, videoId: "videoId", title: "video title"}
+        );
+    });
+
+    test("request change-time", () => {
+        const request = { msg: 'change-time', timestamp: 5};
+        const sender = {};
+        const response = null;
+
+        listenMessages();
+        chrome.runtime.onMessage.callListeners(request, sender, response);
+        const video = document.getElementsByTagName("video")[0];
+        expect(video.currentTime).toBe(5);
+    });
+});
