0dd20105dbccbc41a84284bde994db5ac72ff833
Brian Lin <brianlin.software@gmail.com>
2021-08-29 18:22:43 +1200

    Add clear search button
    Allow user to clear search with a button. The clear search button
    replaces the search icon when there is an active search query. This
    function improves UX by making it easier to clear out a search.

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -5,12 +5,20 @@
             placeholder="Title Search" v-model="searchQuery"
             title="Search Videos"
         >
-        <span class="input-group-text">
+
+        <button
+            type="button" :class="buttonColor"
+            :disabled="!isSearching" @click="clearSearch"
+        >
             <!-- search icon -->
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <svg v-if="!isSearching" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
             </svg>
-        </span>
+            <!-- cancel search icon -->
+            <svg v-else xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                <path d="M1.293 1.293a1 1 0 0 1 1.414 0L8 6.586l5.293-5.293a1 1 0 1 1 1.414 1.414L9.414 8l5.293 5.293a1 1 0 0 1-1.414 1.414L8 9.414l-5.293 5.293a1 1 0 0 1-1.414-1.414L6.586 8 1.293 2.707a1 1 0 0 1 0-1.414z"/>
+            </svg>
+        </button>
     </div>
 </template>
 
@@ -19,6 +27,11 @@
 
 <script>
 export default {
+    methods: {
+        clearSearch() {
+            this.$store.commit('setSearchQuery', "");
+        }
+    },
     computed: {
         // two way binding to vuex search module.
         // need this solution since vuex is unidirectional.
@@ -32,6 +45,18 @@ export default {
                 // commits by default have global namespace
                 this.$store.commit('setSearchQuery', newQuery);
             }
+        },
+        isSearching() {
+            return this.searchQuery != '';
+        },
+        buttonColor() {
+            return [
+                "btn", "shadow-none",
+                {
+                    "btn-danger": this.isSearching,
+                    "btn-secondary": !this.isSearching
+                }
+            ]
         }
     }
 }
