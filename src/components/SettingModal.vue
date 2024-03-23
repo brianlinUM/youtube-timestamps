33b65abfbd6189c5db765890f3dd85a4e3713823
Brian Lin <brianlin.software@gmail.com>
2024-03-23 14:42:54 +1300

    Display no video txt and color in delete all btn

--- src/components/SettingModal.vue
+++ src/components/SettingModal.vue
@@ -10,10 +10,10 @@
         <div class="modal-body py-2">
           <p>Delete Button:</p>
           <button
-            type="button" class="btn btn-sm btn-danger ml-3" data-bs-dismiss="modal"
+            type="button" :class="`btn btn-sm ml-3 ${deleteColor}`"
             @click="removeAllDataSynced" :disabled="isNoVideos"
           >
-            Remove all videos
+            {{ deleteText }}
           </button>
         </div>
         <div class="modal-footer py-2">
@@ -45,6 +45,12 @@ export default {
     ...mapState({
       videos: (state) => state.videosStore.videos,
     }),
+    deleteText() {
+      return this.isNoVideos ? 'No Videos' : 'Delete All';
+    },
+    deleteColor() {
+      return this.isNoVideos ? 'btn-secondary' : 'btn-danger';
+    },
   },
   methods: {
     ...mapActions(['removeAllDataSynced']),
